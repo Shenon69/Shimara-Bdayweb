f6733d9fdc096ac477c0693fd7f1dac44e950709
Shenon69 <ask.tikki69@gmail.com>
2023-03-12 23:11:24 +0530

    update

--- src/Footer.js
+++ src/Footer.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <footer className='bg-[#e6c2ac84] w-[100%]'>
+    <footer className="bg-[#e6c2ac84] w-[100%]">
+      <div>
         <div>
-         <div>
-            <h1 className='font-bold text-xl text-center p-2'>Created With Love</h1>
-            <p className='text-xs text-center px-5'>This Webite was created by Trishan Phillipsz and Wasath theekshana as a present to Shimara Ferdinandez on her birthday</p>
-         </div>
-         <div>
-            <div>
-                <h1>Trishan Phillipsz</h1>
-                
-            </div>
-         </div>
+          <h1 className="font-bold text-xl text-center p-2">
+            Created With Love
+          </h1>
+          <p className="text-xs text-center px-5">
+            This Webite was created by Trishan Phillipsz and Wasath theekshana
+            as a present to Shimara Ferdinandez on her birthday
+          </p>
         </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
