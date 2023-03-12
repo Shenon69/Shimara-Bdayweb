0cd35007db53c39f4deef8a32aea6a55414ccc64
Shenon69 <ask.tikki69@gmail.com>
2023-03-12 23:26:06 +0530

    update footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,20 +2,34 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-[#e6c2ac84] w-[100%]">
+    <footer className="bg-[#e6c2ac84] w-[100%] h-[20vh]">
       <div>
-        <div>
+        <div className="p-5">
           <h1 className="font-bold text-xl text-center p-2">
             Created With Love
           </h1>
           <p className="text-xs text-center px-5">
-            This Webite was created by Trishan Phillipsz and Wasath theekshana
+            This Webite was created by{" "}
+            <a
+              href="https://www.instagram.com/tikkioninsta/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Trishan Phillipsz
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://www.instagram.com/wasath.theekshana/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Wasath theekshana
+            </a>
             as a present to Shimara Ferdinandez on her birthday
           </p>
         </div>
-        <div>
-          <div></div>
-        </div>
+        <div></div>
       </div>
     </footer>
   );
