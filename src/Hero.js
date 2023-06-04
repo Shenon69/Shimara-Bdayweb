af468f4611f85b8815e211a7778f5defc9420068
Shenon69 <ask.tikki69@gmail.com>
2023-06-04 19:24:56 +0530

    update: hero wish

--- src/Hero.js
+++ src/Hero.js
@@ -11,10 +11,10 @@ function Hero() {
         viewport={{ once: true }}
         className="text-4xl font-bold md:text-6xl lg:text-8xl lg:pl-10 lg:max-w-[80%]"
       >
-        Happy Birthday To The <span className="text-white">Best</span>
+        All the best to your exams <span className="text-white">Princess</span>!
       </m.h1>
-      <div className="flex flex-col">
-        <div className="w-[100%]">
+      <div className="flex flex-col lg:flex-row">
+        <div className="w-auto">
           <m.img
             initial={{ y: 100, opacity: 0 }}
             transition={{ duration: 1.5 }}
@@ -22,7 +22,7 @@ function Hero() {
             viewport={{ once: true }}
             src="/heroImg.png"
             alt="hero"
-            className="w-[90%] mx-auto mt-4 md:w-[50%] lg:w-[27%] lg:absolute lg:right-[15%] lg:top-[20%]"
+            className="w-[70%] mx-auto mt-4 md:w-[50%] lg:w-[60%] "
           />
         </div>
         <div className="lg:max-w-[50%] lg:mt-10 lg:pl-10">
@@ -33,21 +33,25 @@ function Hero() {
             viewport={{ once: true }}
           >
             {" "}
-            There might be many things that distance can prevent us from doing
-            together, but it can&apos;t stop us from celebrating your birthday!
-            We&apos;ll always find a way to connect and celebrate, be it a fun
-            little conversation or simply a chat over the phone, we&apos;ll
-            commemorate your special day in our own way! Can&apos;t wait to see
-            you again my food partner. Don&apos;t forget to bring me
-            loaaaaaaaads of chocolate…. LOL! Well so yeah now I&apos;m just
-            typing cause I have to fill the space to make the website look nice
-            and cool… you know I&apos;m not good at writing, hehe. So sorry if I
-            have made any mistakes.
+            As you embark on your AL exams, I wanted to send you a heartfelt
+            message of encouragement and support. <br /> You are about to
+            showcase your incredible knowledge and abilities, and I have no
+            doubt that you will soar through each paper with grace and
+            brilliance. Remember, after you complete each exam, take a moment to
+            celebrate your accomplishments and let go of any lingering doubts or
+            worries. Trust in your preparation and the hard work you've put in.
+            Each paper is an opportunity for you to shine, and you have the
+            power to seize it. I believe in you wholeheartedly, and I want you
+            to know that you are not alone in this journey. Lean on your
+            strength, resilience, and determination to conquer every challenge
+            that comes your way. Stay focused, remain calm, and let your passion
+            guide you. Go out there and ace these exams, my love. Your future
+            awaits, and I am here, cheering you on every step of the way.
           </m.p>
 
-          <button className="border-none rounded-[4rem] bg-[#4F0000] text-white px-2.5 py-1.5 text-[0.9rem] mt-4 lg:mt-10">
+          {/* <button className="border-none rounded-[4rem] bg-[#4F0000] text-white px-2.5 py-1.5 text-[0.9rem] mt-4 lg:mt-10">
             Hit Shen
-          </button>
+          </button> */}
         </div>
       </div>
     </section>
